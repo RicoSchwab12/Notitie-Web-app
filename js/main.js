bb5242223fbb7175275745222f77f5b4398dd99c
Rico Schwab <ricoschwab5@icloud.com>
2017-11-21 13:26:51 +0100

    Commit #3
    Begin gemaakt met de JavaScript.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Main.js
+//Created by Rico
+
+console.log("[R]-Notes Core v1.0");
+
+function showNote(){
+    let pageContent = "Hallo!";
+    const containerDiv = document.getElementById("container");
+    containerDiv.innerHTML = pageContent;
+}
