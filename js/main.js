2e754611d4d3b927814c1ae8cc44a622d7877ec4
Rico Schwab <ricoschwab5@icloud.com>
2017-11-21 15:47:39 +0100

    Commit #5
    Notities inladen vanaf de startpagina werkt nu naar behoren.
    Morgen de mogelijkheid om zelf notities te maken

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 //Created by Rico
 
 console.log("[R]-Notes Core v1.0");
-
+renderNotes();
 function getNoteData(infotype, noteID){
     switch (infotype){
         case "title":
@@ -24,15 +24,27 @@ function getNoteData(infotype, noteID){
 function createTestNote(){
     localStorage.setItem("noteTitle-ID-1", btoa("Test Notitie"));
     localStorage.setItem("noteDescription-ID-1", btoa("Dit is een test notitie die lokaal is opgeslagen.<br>Wanneer je dit ziet werkt alles naar behoren."));
-    console.log("Test items aangemaakt op ID 1. Bestaande notitie is mogelijk overschreven.")
+    localStorage.setItem("noteCount" , parseInt(localStorage.getItem('noteCount')) + 1);
+    localStorage.setItem("noteTitle-ID-2", btoa("Test Notitie 2"));
+    localStorage.setItem("noteDescription-ID-2", btoa("Dit is een test hele andere notitie die lokaal is opgeslagen.<br>Wanneer je dit ziet werkt alles naar behoren."));
+    localStorage.setItem("noteCount" , parseInt(localStorage.getItem('noteCount')) + 1);
+    console.log("Test items aangemaakt op ID 1 en ID 2. Bestaande notitie is mogelijk overschreven.");
 }
 
-function showNote(){
-    let pageContent = "<div class='is-not-selectable' onclick='showHomepage()'>Terug</div><div class='container--title'>" + getNoteData("title", "1") + "</div>" + getNoteData("description", "1");
+function showNote(id){
+    let pageContent = "<div class='is-not-selectable' onclick='showHomepage()'>Terug</div><div class='container--title'>" + getNoteData("title", id) + "</div>" + getNoteData("description", id);
     const containerDiv = document.getElementById("container");
     containerDiv.innerHTML = pageContent;
 }
 
 function showHomepage(){
     window.location = window.location;
+}
+
+function renderNotes(){
+    const noteCount = parseInt(localStorage.getItem('noteCount')) + 1;
+    const notesDiv = document.getElementById('notes');
+    for (var i = 1; i < noteCount; i++) {
+        notesDiv.innerHTML = notesDiv.innerHTML + "<div class='container__item is-not-selectable cursor-is-pointer' onclick='showNote("+i+")' unselectable='on'><span class='container__item--title'>" + getNoteData("title", i) + "</span><br><span class='container__item--desc'>Dit is een voorbeeld notitie.</span></div>";
+    }
 }
